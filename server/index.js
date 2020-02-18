08bfcb34d1307e6e1ff471ca5adfb345d071b2ad
William Tong <Billy@Wills-MacBook-Air.local>
2020-02-18 16:11:23 -0500

    heroku deployment checklist

--- server/index.js
+++ server/index.js
@@ -5,4 +5,5 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000; // dynamic port binding - use env var in Heroku, and port 5000 locally
+app.listen(PORT);

6cc1f0443bf8f00a7ac1f07dd100c385a8e08883
William Tong <Billy@Wills-Air.fios-router.home>
2020-05-06 23:33:54 -0400

    heroku build

--- index.js
+++ index.js
@@ -20,5 +20,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // make sure Express serves production assets
+  app.use(express.static('client/build'));
+  // make sure Express serves index.html if unrecognized route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000; // dynamic port binding - use env var in Heroku, and port 5000 locally
 app.listen(PORT);

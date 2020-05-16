21c72d4b09f44d3d3d3d41001264f10de7a38e29
William Tong <Billy@Wills-Air.fios-router.home>
2020-05-16 17:01:54 -0400

    remove href console error

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ const Dashboard = () => (
   <div>
     Dashboard
     <div className="fixed-action-btn">
-      <a className="btn-floating btn-large red">
+      <a className="btn-floating btn-large red" href="/">
         <i className="material-icons">add</i>
       </a>
     </div>

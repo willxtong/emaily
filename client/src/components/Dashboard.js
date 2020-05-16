0d435e22dcba0b2ac79967a61a5a9441a953e262
William Tong <Billy@Wills-Air.fios-router.home>
2020-05-16 16:59:27 -0400

    dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div>
+    Dashboard
+    <div className="fixed-action-btn">
+      <a className="btn-floating btn-large red">
+        <i className="material-icons">add</i>
+      </a>
+    </div>
+  </div>
+);
+
+export default Dashboard;
